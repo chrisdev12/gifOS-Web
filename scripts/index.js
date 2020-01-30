31c85a8ee5201918de0f85131f9ab91661a1033c
chrisdev12 <christianm.bravop@gmail.com>
2020-01-29 21:44:00 -0500

    pre-maqueteado y estructura de folders (con base en ATOMIC CSS)

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// let mayores = 0;
+// do {
+//     var edades = Number((prompt("Coloque su edad")));
+//     if ((edades*0) != 0 ) {
+//         alert('Por favor verifique que esta colocando un número');
+//     } else {
+//         if (parseInt(edades) >= 18) {
+//             mayores++;
+//         }
+//     }
+// } while (edades != 0);
+    
+//     alert(`Hay ${mayores} mayor(es) de edad en los registrados`)
+// ;
+
