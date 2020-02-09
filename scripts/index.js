b6c9e81f58900079c24eb031f023186725be8b45
chrisdev12 <christianm.bravop@gmail.com>
2020-02-09 10:36:46 -0500

    First random gifts that are showed implemented | Gifts through searchbar implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,20 @@
 const apikey = 'FomuiY9mN225fs0Hy6WPQTYPrumdf6R8';
 const searchbar = document.querySelector('#searchbar');
 const searchButton = document.querySelector('nav button');
-const mainSection = document.querySelector('main');
+const trendSectionGifts = document.querySelectorAll('#trends img');
+const mainSectionGifts = document.querySelectorAll('#suggestions img');
+const stylelink = document.querySelector('head link');
 
+//General API request: Working with search and random endpoints.
+//NOTE: The gifts URL are on the JSON > images > original > url
 
-
-searchButton.addEventListener('click', function () {
+function searchGifts(search,endpoint,limit) {
     
-    let newDisplay = '';
-    searchGift(searchbar.value)
-        .then((content) => {
-            content.forEach(element => {
-                newDisplay += `<img src="${element.images.original.url}" class="gift"></img>`           
-            });
-            
-            mainSection.innerHTML = newDisplay;
-        });
-});
-
-//The gifts URL are on the JSON gif > images > url
-
-//request via search endpoint.
-function searchGift(search) {
-    search.toLowerCase();
-    
-    let a = fetch(`http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${search}`) 
+    let a = fetch(`http://api.giphy.com/v1/gifs/${endpoint}?api_key=${apikey}&q=${search}&limit=${limit}`) 
         .then((response) => {
             return response.json();
             })
             .then((myJson) => {
-                result = myJson.data;
                 return myJson.data;
             })
             .catch((error) => {
@@ -37,4 +22,58 @@ function searchGift(search) {
             })
     
     return a;
+}
+
+
+searchButton.addEventListener('click', function () {
+    let trendArray = [];
+    searchGifts(searchbar.value,'search',10)
+        .then((content) => {
+            content.forEach(element => {
+                trendArray.push(element.images.original);          
+            });
+        
+            for (let i = 0; i < trendArray.length; i++){
+                trendSectionGifts[i].setAttribute('src', `${trendArray[i].url}`)
+            };
+            
+            window.location.href = "#trends";
+        })
+        .catch((error) => {
+            return error;
+        })
+});
+
+//First APi request, do the first populate of gifsts that are visible for 
+//first time in the page
+
+displayRandomGifts();
+
+function displayRandomGifts() {
+
+    let randomArray = [];
+    
+    searchGifts(null, 'trending', 14)
+        .then((content) => {
+            console.log(content);
+            content.forEach(element => {
+                randomArray.push(element.images.original);          
+            });
+            
+            let x = 0; //Iterator for trendSectionGifts that are in another section and
+            //begin from 0
+        
+            for (let i = 0; i < randomArray.length; i++){
+                
+                if (i < 4) {
+                    mainSectionGifts[i].setAttribute('src', `${randomArray[i].url}`)
+                } else {
+                    trendSectionGifts[x].setAttribute('src', `${randomArray[i].url}`)
+                    x++
+                }
+            };
+        })
+        .catch((error) => {
+            return error;
+        })
 }
