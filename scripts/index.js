6462047910aa993a66cc848fddc8b093ee46ac74
chrisdev12 <christianm.bravop@gmail.com>
2020-03-30 19:52:20 -0500

    Index HTML optimization: remove all the queryselector unnecesary and implmented async/await on promises

--- scripts/index.js
+++ scripts/index.js
@@ -1,115 +1,97 @@
 const apikey = 'FomuiY9mN225fs0Hy6WPQTYPrumdf6R8';
 const searchbar = document.getElementById('searchbar');
-const searchButton = document.querySelector('nav button');
-const trendSectionGifts = document.querySelectorAll('#trends img');
-const mainSectionGifts = document.querySelectorAll('.suggest-gifts');
-const stylelink = document.querySelector('head link');
-const titleSuggestGifts = document.querySelectorAll('.suggestion-container p');
-const titleTrendGifts = document.querySelectorAll('.trend-container p')
+const searchButton = document.getElementById('searchBtn');
+const trendSectionGifts = document.getElementsByClassName('trend-img');
+const mainSectionGifts = document.getElementsByClassName('suggest-gifts');
+const stylelink = document.getElementsByTagName('link')[0];
+const titleSuggestGifts = document.getElementsByClassName('suggestion-text');
+const titleTrendGifts = document.getElementsByClassName('trend-text')
 const mainButtons = document.querySelectorAll('main button');
 
 //General API request: Working with search and random endpoints.
-//NOTE: The gifts URL are on the JSON > images > original > url
+//NOTE: The gifts URL are on the JSON > data > images > original > url
 
-function searchGifts(search,endpoint,limit) {
-    
-    let req = fetch(`http://api.giphy.com/v1/gifs/${endpoint}?api_key=${apikey}&q=${search}&limit=${limit}`) 
-        .then((response) => {
-            return response.json();
-            })
-            .then((myJson) => {
-                return myJson.data;
-            })
-            .catch((error) => {
-                return error;
-            })
-    
-    return req;
+async function searchGifts(search,endpoint,limit) {
+    try {
+        let req = fetch(`http://api.giphy.com/v1/gifs/${endpoint}?api_key=${apikey}&q=${search}&limit=${limit}`) 
+        let res = await req
+        let myJson = await res.json()       
+        return myJson.data;  
+    } catch (err) {
+        throw new Error(err)
+    }
 }
 
-
-searchButton.addEventListener('click', function () {
-    let trendArray = [];
-    searchGifts(searchbar.value,'search',10)
-        .then((content) => {
-            content.forEach(element => {
-                trendArray.push(element.images.original);          
-            });
-        
-            for (let i = 0; i < trendArray.length; i++){
-                trendSectionGifts[i].setAttribute('src', `${trendArray[i].url}`)
-            };
-            
-            window.location.href = "#trends";
-        })
-        .catch((error) => {
-            return error;
-        })
+searchButton.addEventListener('click', () => {
+    searchMoreGifos(searchbar.value)
 });
 
-//First APi request, do the first populate of gifsts that are visible for 
-//first time in the page
+//------------Main buttons search---------------
+mainButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        searchMoreGifos(event.target.value)
+    });
+});
+
+//First APi request:  populate the home with random gifos.
 
 displayRandomGifos();
 
-function displayRandomGifos() {
-
-    let randomGifos = []; //List of objects that contains the URL and title of each random gift
+async function displayRandomGifos() {
+    try {
+        let randomGifos = []; //List of objects that contains the URL and title of each random gift
     
-    searchGifts(null, 'trending', 14)
-        .then((content) => {
-            content.forEach(element => {
-                let giftText = element.title.split(' ').slice(0,4); //Get max 4 words
-                let hashtags = '';
-                giftText.forEach(element =>{ 
-                    hashtags += `#${element} `;
-                })
-                
-                let gifo = new Object(); //Create an object to organize the URL, title and text to shon on containers.
-                gifo.url = element.images.original.url;
-                gifo.text = `${hashtags}` || '#Not #Gift #Title #Available';
-                gifo.value = element.title;
-                randomGifos.push(gifo);         
-            });
+        let res = await searchGifts(null, 'trending', 14)   
+        res.forEach(element => {
+            let giftText = element.title.split(' ').slice(0,4); //Get max 4 words
+            let hashtags = '';
+            giftText.forEach(element =>{ 
+                hashtags += `#${element} `;
+            })
             
-            let x = 0; //Iterator for gifos of trendSectionGifts that are in another section/container  
-            for (let i = 0; i < randomGifos.length; i++){
+            let gifo = new Object(); //Create an object to organize the URL, title and text to shon on containers.
+            gifo.url = element.images.original.url;
+            gifo.text = `${hashtags}` || '#Not #Gift #Title #Available';
+            gifo.value = element.title;
+            randomGifos.push(gifo);         
+        });
                 
-                if (i < 4) {
-                    mainSectionGifts[i].setAttribute('src', `${randomGifos[i].url}`);
-                    mainButtons[i].setAttribute('value', `${randomGifos[i].value}`);
-                    titleSuggestGifts[i].textContent = `${randomGifos[i].text}`;
-                } else {
-                    trendSectionGifts[x].setAttribute('src', `${randomGifos[i].url}`);
-                    titleTrendGifts[x].textContent = `${randomGifos[i].text}`;
-                    x++
-                }
-            };
-        })
-        .catch((error) => {
-            return error;
-        })
+        let x = 0; //Iterator for gifos of trendSectionGifts that are in another section/container  
+        for (let i = 0; i < randomGifos.length; i++){
+            
+            if (i < 4) {
+                mainSectionGifts[i].setAttribute('src', `${randomGifos[i].url}`);
+                mainButtons[i].setAttribute('value', `${randomGifos[i].value}`);
+                titleSuggestGifts[i].textContent = `${randomGifos[i].text}`;
+            } else {
+                trendSectionGifts[x].setAttribute('src', `${randomGifos[i].url}`);
+                titleTrendGifts[x].textContent = `${randomGifos[i].text}`;
+                x++
+            }
+        };
+        
+    } catch (err) {
+        throw new Error(err)
+    }
 }
 
-//------------Main buttons search---------------
-
-mainButtons.forEach(element => {
-    element.addEventListener('click', function () {
+async function searchMoreGifos(event) {
+    try {
         let trendArray = [];
-        searchGifts(event.target.value, 'search', 10)
-            .then((content) => {
-                content.forEach(element => {
-                    trendArray.push(element.images.original);
-                });
-        
-                for (let i = 0; i < trendArray.length; i++) {
-                    trendSectionGifts[i].setAttribute('src', `${trendArray[i].url}`)
-                };
-            
-                window.location.href = "#trends";
-            })
-            .catch((error) => {
-                return error;
-            })
-    });
-})
+        let trendTextArray = [];
+        let gifs = await searchGifts(event, 'search', 10)
+        gifs.forEach(element => {
+            trendArray.push(element.images.original);
+            trendTextArray.push(element.title);
+        });
+    
+        for (let i = 0; i < trendArray.length; i++) {
+            trendSectionGifts[i].setAttribute('src', `${trendArray[i].url}`)
+            titleTrendGifts[i].textContent = `${trendTextArray[i]}`;
+        };
+    
+        window.location.href = "#trends";
+    } catch (err) {
+        throw new Error(err)
+    }  
+}
--- scripts/typehead.js
+++ scripts/typehead.js
@@ -4,36 +4,35 @@ let searchCoincidencesContainer = document.createElement('div');
 //Add the style that will define the style of our container
 searchCoincidencesContainer.setAttribute('class', 'coincidences');
 
-//Event to get new coincideces once time a new key being pressed
-searchbar.addEventListener('keyup', function () {
-    let textSearching = event.target.value;
-    fetch(`http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${textSearching}&limit=10`) 
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            let i = 0; //Iterator; We only should display the fist 3 matches;
-            searchCoincidencesContainer.innerHTML = ''; //Refresh the result in each search;
-            myJson.data.forEach(element => {
-                let regex = new RegExp(`${textSearching.toLowerCase()}`);
-                if (regex.test(element.title.toLowerCase()) && i < 3) {
-                    searchCoincidencesContainer.innerHTML += `<p>${element.title}</p>`;
-                    i++;
-                }
-            });
-            navcontainer.appendChild(searchCoincidencesContainer);
-            addingCoincidence();
-            monitorCoincidenceContainer();
-            return myJson.data;
-        })
-        .catch((error) => {
-            return error;
-        })
-});
+
+searchbar.addEventListener('keyup', typhead);
+
+//Event to get new coincideces regard the letter that is being input(new key being pressed).
+async function typhead() {
+    try {
+        let textSearching = event.target.value;
+        let req = fetch(`http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${textSearching}&limit=10`)
+        let res = await req
+        let myJson = await res.json()
+        let i = 0; //Iterator; We only should display the fist 3 matches;
+        searchCoincidencesContainer.innerHTML = ''; //Refresh the result in each search;  
+        myJson.data.forEach(element => {
+            let regex = new RegExp(`${textSearching.toLowerCase()}`);
+            if (regex.test(element.title.toLowerCase()) && i < 3) {
+                searchCoincidencesContainer.innerHTML += `<p>${element.title}</p>`;
+                i++;
+            }
+        });
+        navcontainer.appendChild(searchCoincidencesContainer);
+        addingCoincidence();
+        monitorCoincidenceContainer();   
+    } catch (err) {
+        throw new Error(err)
+    }
+}
 
 
 //Add desired coincidence to searchbar content
-
 function addingCoincidence(){
     const coincidence = document.querySelectorAll('.coincidences p');
     coincidence.forEach(element =>
