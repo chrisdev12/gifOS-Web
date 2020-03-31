19892d876f2060e6eecaa50c2b74f8d6fbd5f8bb
chrisdev12 <christianm.bravop@gmail.com>
2020-03-31 09:26:03 -0500

    Organize scripts and changed the select for a div that will have the same functionality  as a select with JS

---
+++ scripts/index/changemode.js
@@ -0,0 +1,32 @@
+// const themechanger = document.getElementById('selectTheme');
+// //imgTheme: Logo and lupe images
+// const imgTheme = document.getElementsByClassName('img-theme');
+// const stylelink = document.getElementsByTagName('link')[0];
+// let activeTheme = 'white'; // know the current theme 
+    
+// themechanger.addEventListener('change', function () {
+    
+//     switch (event.target.value) {
+//         case 'white': stylelink.setAttribute('href', './styles/index/whitemode.css')
+//             imgTheme[0].setAttribute('src', './images/gifOF_logo.png');
+//             imgTheme[1].setAttribute('src', './images/lupa_inactive.svg');
+//             activeTheme = 'white';
+//             break;
+//         case 'dark': stylelink.setAttribute('href', './styles/index/darkmode.css')
+//             imgTheme[0].setAttribute('src', './images/gifOF_logo_dark.png');
+//             imgTheme[1].setAttribute('src', './images/CombinedShape.svg');
+//             activeTheme = 'dark';
+//             break;    
+//     }
+// });
+
+{/* <span class="select-container header-btn hover-border">
+    <select id="selectTheme">
+        <option value="">Elegir tema</option>
+        <option value="white">Modo claro</option>
+        <option value="dark">Modo oscuro</option>
+    </select>
+    <span>
+        <img src="./images/dropdown.svg" alt="dropdown icon">
+    </span>
+</span> */}
